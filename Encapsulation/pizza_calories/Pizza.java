f70eb8895aad8cbdffcaa52c24d5e99b53f2be3b
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-09 22:41:03 +0200

    Create Pizza.java

---
+++ Encapsulation/pizza_calories/Pizza.java
@@ -0,0 +1,62 @@
+package pizza_calories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+
+    private String name ;
+    private  int numberOfToppings;
+    private Dough dough;
+    private List<Topping> toppings;
+
+
+    public Pizza(String name, int numberOfToppings) {
+
+        setName(name);
+        setToppings(numberOfToppings);
+        this.numberOfToppings = numberOfToppings; // така се прави зада може стойността на numberOfToppingsда се промени
+        // ,защото setToppings връща не броя а new ArrayList<>
+    }
+
+    private void setToppings(int toppings) {
+        if (toppings < 0 || toppings > 10) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+        this.toppings = new ArrayList<>(toppings);
+    }
+
+    private void setName(String name) {
+        if (name.trim().isEmpty() || name.length() > 15) {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+
+        this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addTopping(Topping topping) {
+        if (this.toppings.size() == this.numberOfToppings) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+        this.toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        double calcToppings = this.toppings.stream().mapToDouble(Topping::calculateCalories).sum();
+//        for (Topping topping : toppings) {
+//     calcToppings += topping.calculateCalories();
+// }
+        return this.dough.calculateCalories() + calcToppings;
+    }
+
+
+
+}
