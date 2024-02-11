41f03b29e2396ea3f9ab09a7ff8e31ba1637b45f
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-11 23:01:56 +0200

    Create Truck.java

---
+++ Polymorphism/p1_vehicles/Truck.java
@@ -0,0 +1,7 @@
+package p1_vehicles;
+
+public class Truck extends VehicleImpl{
+    public Truck(double fuelQuantity, double fuelConsumption) {
+        super(fuelQuantity, fuelConsumption);
+    }
+}
