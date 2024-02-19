c377db4855e19a04b1653bf10a6ee78c7b87339b
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-19 22:37:23 +0200

    а

---
+++ Polymorphism/P2_Vehicle_Extension/Truck.java
@@ -0,0 +1,13 @@
+package P2_Vehicle_Extension;
+
+public class Truck extends VehicleImpl{
+    public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+        super.setFuelConsumption(fuelConsumption + 1.6);
+    }
+
+    @Override
+    public void refuel(double liters) {
+        super.refuel(liters * 0.95);
+    }
+}
