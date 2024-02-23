d046b43983cfaa295f34026d9545659e5a8b6777
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-23 22:58:35 +0200

    Create BiscuitCake.java

---
+++ DesingPatterns/factory/BiscuitCake.java
@@ -0,0 +1,24 @@
+package factory.factoryExercise;
+
+public class BiscuitCake extends Cake{
+    public BiscuitCake(double diameter, double price, int pieces) {
+        super(diameter, price, pieces);
+    }
+
+    @Override
+    public void prepare() {
+        System.out.println("Preparing Biscuit Cake...");
+    }
+
+    @Override
+    public void bake() {
+        System.out.println("Baking Biscuit Cake...");
+    }
+
+    @Override
+    public void box() {
+        System.out.println("Boxing Biscuit Cake...");
+        System.out.println("Your Biscuit Cake is ready");
+        System.out.printf("Your bill is %.2f", price * pieces);
+    }
+}
