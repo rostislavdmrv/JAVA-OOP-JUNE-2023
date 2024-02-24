004c9900d0caaac8fffb6953fe6901cfbbb55236
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-24 23:19:50 +0200

    Create PastryShop.java

---
+++ DesingPatterns/factory/PastryShop.java
@@ -0,0 +1,15 @@
+package factory.factoryExercise;
+
+public class PastryShop {
+    private PastryShop() {
+    }
+
+    public static Cake orderCake(String cakeType, double diameter, double price, int pieces) {
+        Cake cake = CakeFactory.createCake(cakeType, diameter, price, pieces);
+        cake.prepare();
+        cake.bake();
+        cake.box();
+
+        return cake;
+    }
+}
