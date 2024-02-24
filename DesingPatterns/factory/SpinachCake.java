c60e30469b164da6a7e05607416353e9be24a21c
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-24 23:20:38 +0200

    Create SpinachCake.java

---
+++ DesingPatterns/factory/SpinachCake.java
@@ -0,0 +1,24 @@
+package factory.factoryExercise;
+
+public class SpinachCake extends Cake{
+    public SpinachCake(double diameter, double price, int pieces) {
+        super(diameter, price, pieces);
+    }
+
+    @Override
+    public void prepare() {
+        System.out.println("Preparing Spinach Cake...");
+    }
+
+    @Override
+    public void bake() {
+        System.out.println("Baking Spinach Cake...");
+    }
+
+    @Override
+    public void box() {
+        System.out.println("Boxing Chocolate Cake...");
+        System.out.println("Your Chocolate Cake is ready");
+        System.out.printf("Your bill is %.2f", price * pieces);
+    }
+}
