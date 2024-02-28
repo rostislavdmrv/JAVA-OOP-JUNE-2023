cf9f2acc7bc0e2f1afb39ec26e819c4191855756
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-02-28 23:35:59 +0200

    Create PayPalPayment.java

---
+++ DesingPatterns/strategy/PayPalPayment.java
@@ -0,0 +1,16 @@
+package strategy;
+
+public class PayPalPayment implements Payment{
+    private String emailId;
+    private String password;
+
+    public PayPalPayment(String email, String pwd) {
+        this.emailId = email;
+        this.password = pwd;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using Paypal.");
+    }
+}
